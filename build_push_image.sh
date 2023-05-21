e2ce9b79e2934f07043b1f2333eb655b87ddc01d
Farezas <fareza087@gmail.com>
2023-05-21 22:00:17 +0700

    revision

--- build_push_image.sh
+++ build_push_image.sh
@@ -14,7 +14,7 @@ docker image ls
 
 # re-tag image
 echo "-------->\n\nchange image name\n"
-docker tag item-app:v1 ghcr.io/$USERNAME/a433-microservices/item-app:latest
+docker tag item-app:v1 ghcr.io/$USERNAME/a433-microservices/item-app:v1
 
 # lakukan login ke github container registry
 echo "-------->\n\ngithub registry login\n"
@@ -22,4 +22,4 @@ echo $token | docker login ghcr.io -u $USERNAME --password-stdin
 
 # push image yang sudah di re-tag ke ghcr
 echo "-------->\n\npush image\n"
-docker push ghcr.io/$USERNAME/a433-microservices/item-app:latest
+docker push ghcr.io/$USERNAME/a433-microservices/item-app:v1
